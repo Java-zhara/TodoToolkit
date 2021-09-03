cd82f91feb452d1fd1ca4644db1224d3c32a3687
Java-zhara <java-zhara@mail.ru>
2021-09-03 15:36:37 +0300

    todo toolkit

--- src/App.js
+++ src/App.js
@@ -1,45 +1,25 @@
 import { useState } from "react";
+import { useDispatch } from "react-redux";
 import "./App.css";
+import InputField from "./components/InputField";
+import TodoList from "./components/TodoList";
+import { addTodo } from "./store/todoSlice";
 
 function App() {
-  const [todos, setTodos] = useState([]);
-  const [text, setText] = useState([]);
+  const [text, setText] = useState("");
+  const dispatch = useDispatch();
 
-  const addTodo = () => {
+  const addTask = () => {
     if (text.trim().length) {
-      setTodos([
-        ...todos,
-        {
-          id: new Date().toISOString,
-          text,
-          completed: false,
-        },
-      ]);
+      dispatch(addTodo({ text }));
       setText("");
     }
   };
 
   return (
     <div className="App">
-      <label>
-        <input
-          value={text}
-          onChange={(e) => {
-            setText(e.target.value);
-          }}
-        />
-        <button onClick={addTodo}>Add Todo</button>
-      </label>
-
-      <ul>
-        {todos.map((todo) => (
-          <li key={todo.id}>
-            <input type="checkbox" />
-            <span>{todo.text}</span>
-            <span>&times;</span>
-          </li>
-        ))}
-      </ul>
+      <InputField text={text} handleInput={setText} handleSubmit={addTask} />
+      <TodoList />
     </div>
   );
 }
